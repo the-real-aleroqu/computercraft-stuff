0aa00319f715b541b905b1c8d22994054e108197
Alex <97312894+the-real-aleroqu@users.noreply.github.com>
2022-11-15 22:46:15 +0000

    Added coolant bar and method to get color

--- fission.lua
+++ fission.lua
@@ -1,8 +1,19 @@
 local basalt = require("basalt")
 local monitor = peripheral.find("monitor")
-local reactor = peripheral.find("fissionReactorLogicAdapter")
+local adapter = peripheral.find("fissionReactorLogicAdapter")
+
 if monitor == nil then error("Could not find monitor!") end
-if reactor == nil then error("Could not find reactor logic adapter!") end
+if adapter == nil then error("Could not find reactor logic adapter!") end
+
+local reactor = {
+    coolant = { name = "minecraft:water"}
+}
+
+function reactor:getCoolantColor()
+    if coolant.name == "minecraft:water" then return colors.blue end
+    if coolant.name == "mekanism:sodium" then return colors.lightGray end
+    return colors.white -- Empty
+end
 
 --- Main Frame
 local mainFrame = basalt.createFrame()
@@ -10,10 +21,18 @@ local mainFrame = basalt.createFrame()
                         :setMonitorScale(0.5)
                         :setBackground(colors.black)
                         :setTheme({
-
+                            ProgressbarBG = colors.white,
+                            ProgressbarActiveBG = colors.green
                         })
 
 mainFrame:addProgressBar()
             :setProgress(50)
+
+--- Coolant
+local coolantBar = mainFrame:addProgressBar()
+            :setDirection(3) -- Bottom to Top
+            :setProgress(50)
+            :setProgressBar(reactor:getCoolantColor()) -- Change color to coolant type
+
 
 basalt.autoUpdate()
