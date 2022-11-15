fae26209abe9974eef75f39b24489c4ae7fdfd42
Alex <97312894+the-real-aleroqu@users.noreply.github.com>
2022-11-15 21:19:27 +0000

    Testing Progress Bar

--- fission.lua
+++ fission.lua
@@ -1,12 +1,19 @@
 local basalt = require("basalt")
 local monitor = peripheral.find("monitor")
+local reactor = peripheral.find("fissionReactorLogicAdapter")
+if monitor == nil then error("Could not find monitor!") end
+if reactor == nil then error("Could not find reactor logic adapter!") end
 
+--- Main Frame
 local mainFrame = basalt.createFrame()
                         :setMonitor(peripheral.getName(monitor))
                         :setMonitorScale(0.5)
+                        :setBackground(colors.black)
+                        :setTheme({
 
+                        })
 
-
-
+mainFrame:addProgressBar()
+            :setProgress(50)
 
 basalt.autoUpdate()
