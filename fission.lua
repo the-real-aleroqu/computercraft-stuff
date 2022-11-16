dae3e719e36b1e88beb2f69d96dcb469020435ef
Alex <97312894+the-real-aleroqu@users.noreply.github.com>
2022-11-16 13:57:38 +0000

    Removed Progress Bars, added frame for values

--- fission.lua
+++ fission.lua
@@ -3,17 +3,31 @@ local monitor = peripheral.find("monitor")
 local adapter = peripheral.find("fissionReactorLogicAdapter")
 
 if monitor == nil then error("Could not find monitor!") end
-if adapter == nil then error("Could not find reactor logic adapter!") end
+--if adapter == nil then error("Could not find reactor logic adapter!") end
+
+--[[ monitor.getCoolant() ->
+    {   
+        name = "minecraft:water",
+        amount = 12932986346
+    }
+]]
+
+local _maxCoolant = monitor.getCoolantCapacity()
+local _maxFuel = monitor.getFuelCapacity()
+local _maxHeatedCoolant = monitor.getHeatedCoolantCapacity()
+local _maxWaste = monitor.getWasteCapacity()
 
 local reactor = {
-    coolant = { name = "minecraft:water"}
+    coolant = --[[ { name = "minecraft:water" } ]] monitor.getCoolant(),
+    fuel = monitor.getFuel(),
+    heatedCoolant = --[[ { name = "mekanism:steam" } ]] monitor.getHeatedCoolant(),
+    waste = monitor.getWaste()
 }
 
-function reactor:getCoolantColor()
-    if coolant.name == "minecraft:water" then return colors.blue end
-    if coolant.name == "mekanism:sodium" then return colors.lightGray end
-    return colors.white -- Empty
-end
+--- Values Frame
+local valuesFrame = mainframe:addFrame()
+                            :setPosition(15,1)
+                            :setSize(12,15)
 
 --- Main Frame
 local mainFrame = basalt.createFrame()
@@ -25,14 +39,11 @@ local mainFrame = basalt.createFrame()
                             ProgressbarActiveBG = colors.green
                         })
 
-mainFrame:addProgressBar()
+mainFrame:addProgressbar()
             :setProgress(50)
 
 --- Coolant
-local coolantBar = mainFrame:addProgressBar()
-            :setDirection(3) -- Bottom to Top
-            :setProgress(50)
-            :setProgressBar(reactor:getCoolantColor()) -- Change color to coolant type
+local coolantFrame
 
-
+--- Fuel
 basalt.autoUpdate()
