fc88657b64ff12665763331867ea52ad96909cef
Alex <97312894+the-real-aleroqu@users.noreply.github.com>
2022-11-16 14:14:54 +0000

    Fixed errors

--- fission.lua
+++ fission.lua
@@ -5,12 +5,11 @@ local adapter = peripheral.find("fissionReactorLogicAdapter")
 if monitor == nil then error("Could not find monitor!") end
 --if adapter == nil then error("Could not find reactor logic adapter!") end
 
---[[ monitor.getCoolant() ->
-    {   
-        name = "minecraft:water",
-        amount = 12932986346
-    }
-]]
+-- monitor.getCoolant() ->
+--     {   
+--         name = "minecraft:water",
+--         amount = 12932986346
+--     }
 
 local _maxCoolant = monitor.getCoolantCapacity()
 local _maxFuel = monitor.getFuelCapacity()
@@ -18,16 +17,13 @@ local _maxHeatedCoolant = monitor.getHeatedCoolantCapacity()
 local _maxWaste = monitor.getWasteCapacity()
 
 local reactor = {
-    coolant = --[[ { name = "minecraft:water" } ]] monitor.getCoolant(),
+    --{ name = "minecraft:water" }
+    coolant = monitor.getCoolant(),
     fuel = monitor.getFuel(),
-    heatedCoolant = --[[ { name = "mekanism:steam" } ]] monitor.getHeatedCoolant(),
+    -- { name = "mekanism:steam" }
+    heatedCoolant = monitor.getHeatedCoolant(),
     waste = monitor.getWaste()
 }
-
---- Values Frame
-local valuesFrame = mainframe:addFrame()
-                            :setPosition(15,1)
-                            :setSize(12,15)
 
 --- Main Frame
 local mainFrame = basalt.createFrame()
@@ -39,8 +35,11 @@ local mainFrame = basalt.createFrame()
                             ProgressbarActiveBG = colors.green
                         })
 
-mainFrame:addProgressbar()
-            :setProgress(50)
+
+--- Values Frame
+local valuesFrame = mainframe:addFrame()
+                            :setPosition(15,1)
+                            :setSize(12,15)
 
 --- Coolant
 local coolantFrame
